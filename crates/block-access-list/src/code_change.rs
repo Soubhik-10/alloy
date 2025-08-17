edd8402cc11c9f9b4f72f5030c3b19e84c8e1e3a
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-08-17 11:17:54 +0530

    feat: added new modified core str as per update

--- crates/block-access-list/src/code_change.rs
+++ crates/block-access-list/src/code_change.rs
@@ -1,29 +1,31 @@
 //! Contains the `CodeChange` struct, which represents a new code for an account.
 //! Single code change: `tx_index` -> `new_code`
-use alloy_primitives::{Bytes, TxIndex};
+use alloy_primitives::Bytes;
 use alloy_rlp::{RlpDecodable, RlpEncodable};
 use serde::{Deserialize, Serialize};
+
+use crate::BlockAccessIndex;
 
 /// This struct is used to track the new codes of accounts in a block.
 #[derive(
     Debug, Clone, Default, PartialEq, Eq, RlpDecodable, RlpEncodable, Serialize, Deserialize,
 )]
-pub struct CodeChange {
+pub struct CodeChanges {
     /// The index of the transaction that caused this balance change.
-    pub tx_index: TxIndex,
+    pub block_access_index: BlockAccessIndex,
     /// The new code of the account.
     pub new_code: Bytes,
 }
-impl CodeChange {
+impl CodeChanges {
     /// Creates a new `CodeChange`.
-    pub fn new(tx_index: TxIndex) -> Self {
-        Self { tx_index, new_code: Default::default() }
+    pub fn new(block_access_index: BlockAccessIndex) -> Self {
+        Self { block_access_index, new_code: Default::default() }
     }
 
     /// Returns the transaction index.
     #[inline]
-    pub const fn tx_index(&self) -> TxIndex {
-        self.tx_index
+    pub const fn block_access_index(&self) -> BlockAccessIndex {
+        self.block_access_index
     }
 
     /// Returns the new code.
--- crates/block-access-list/src/lib.rs
+++ crates/block-access-list/src/lib.rs
@@ -1,13 +1,17 @@
 //! Block-level access lists for Reth.
 #![no_std]
 extern crate alloc;
+
+/// Type alias for block index for eip-7928.
+pub type BlockAccessIndex = u16;
+
 /// Module for handling storage changes within a block.
 pub mod storage_change;
 pub use storage_change::*;
 
 /// Module for managing storage slots and their changes.
-pub mod storage_slots;
-pub use storage_slots::*;
+pub mod slot_changes;
+pub use slot_changes::*;
 
 /// Module containing constants used throughout the block access list.
 pub mod constants;
