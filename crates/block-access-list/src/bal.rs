e26c52e8310a7798fec79ed3f6760d84b95a82de
Soubhik-10 <soubhiksmp2004@gmail.com>
2025-09-03 20:24:14 +0530

    serde

--- crates/block-access-list/src/bal.rs
+++ crates/block-access-list/src/bal.rs
@@ -6,22 +6,22 @@ use alloc::vec::Vec;
 /// Vector of account changes.
 pub type BlockAccessList = Vec<AccountChanges>;
 
-#[cfg(test)]
-mod tests {
-    use alloy_primitives::{keccak256, Bytes};
-    use alloy_rlp::{EMPTY_LIST_CODE, EMPTY_STRING_CODE};
+// #[cfg(test)]
+// mod tests {
+//     use alloy_primitives::{keccak256, Bytes};
+//     use alloy_rlp::{EMPTY_LIST_CODE, EMPTY_STRING_CODE};
 
-    use crate::BlockAccessList;
+//     use crate::BlockAccessList;
 
-    #[test]
-    fn test_hash() {
-        // let bal = None;
-        // println!("bal default= {:?}", bal);
-        let bal = BlockAccessList::default();
-        // let rlp_encoded = alloy_rlp::encode("");
-        println!("RLP encoded bal default= {:?}", bal);
-        let hash = keccak256(alloy_rlp::encode(bal));
+//     #[test]
+//     fn test_hash() {
+//         // let bal = None;
+//         // println!("bal default= {:?}", bal);
+//         let bal = BlockAccessList::default();
+//         // let rlp_encoded = alloy_rlp::encode("");
+//         println!("RLP encoded bal default= {:?}", bal);
+//         let hash = keccak256(alloy_rlp::encode(bal));
 
-        println!("hash {:?}", hash);
-    }
-}
+//         println!("hash {:?}", hash);
+//     }
+// }
